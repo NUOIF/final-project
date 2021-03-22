747ca951953e3380f6d58ea5189db9c5de27bcaa
hamad57 <hamadayy123@gmail.com>
2021-03-22 14:26:12 +0300

    delete apps

--- login/admin.py
+++ login/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from .models import Department, Groups, Students, Evaluation, Examiners, Projects, Doctors, CommitteesCharis
+# Register your models here.
+
+admin.site.register(Department),
+admin.site.register(Groups),
+admin.site.register(Students),
+admin.site.register(Evaluation),
+admin.site.register(Examiners),
+admin.site.register(Projects),
+admin.site.register(Doctors),
+admin.site.register(CommitteesCharis)
 
 
 
